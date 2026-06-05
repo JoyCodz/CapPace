a1e8a1c927c0a7c78f497444dd5267c95a191d5a
JoyCodz <joysantra665@gmail.com~>
2026-06-05 14:36:21 +0530

    chore: add morgan for API request logging

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const authRoutes = require('./routes/auth');
 const playlistRoutes = require('./routes/playlists');
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev')); // Logs HTTP requests to the console
 
 // Connect to MongoDB
 console.log("URI CHECK: ", process.env.MONGODB_URI);
